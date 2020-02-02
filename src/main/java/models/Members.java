c40e3a17e421d9a53ed868d154d99c65dab6136c
LennyDennis <lennydennis@gmail.com>
2020-02-02 18:11:28 +0300

    Add methods to the Members class

--- src/main/java/models/Members.java
+++ src/main/java/models/Members.java
@@ -1,4 +1,70 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Members {
+    private String mName;
+    private int mAge;
+    private String mPower;
+    private String mWeakness;
+    private static List<Members> instances = new ArrayList<Members>();
+    private int mId;
+
+    public Members(String name, int age, String power, String weakness) {
+        this.mName = name;
+        this.mAge = age;
+        this.mPower = power;
+        this.mWeakness = weakness;
+        instances.add(this);
+        mId = instances.size();
+    }
+
+    public int getAge() {
+        return mAge;
+    }
+
+    public void setAge(int age) {
+        this.mAge = age;
+    }
+
+    public String getPower() {
+        return mPower;
+    }
+
+    public void setPower(String power) {
+        this.mPower = power;
+    }
+
+    public String getWeakness() {
+        return mWeakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.mWeakness = weakness;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    public static List<Members> all(){
+        return instances;
+    }
+
+    public int getId(){
+        return mId;
+    }
+
+    public static void clearMembers(){
+        instances.clear();
+    }
+
+    public static Members find(int id){
+        return instances.get(id-1);
+    }
 }
