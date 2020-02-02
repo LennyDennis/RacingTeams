c8b1ee6f945aa19eb7b61f0405ab5404ce0a3e52
LennyDennis <lennydennis@gmail.com>
2020-02-02 18:12:03 +0300

    Add methods to Teams class

--- src/main/java/models/Teams.java
+++ src/main/java/models/Teams.java
@@ -1,5 +1,8 @@
 package models;
 
+import org.eclipse.jetty.server.PushBuilder;
+
+import java.lang.reflect.Member;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,36 +14,71 @@ public class Teams {
     private static List<Teams> instances = new ArrayList<>();
     private List<Members> mMembers;
 
-    public Teams(String mName, String mCause, int mNumber) {
-        this.mName = mName;
-        this.mCause = mCause;
-        this.mNumber = mNumber;
+    public Teams(String name, String cause, int number) {
+        this.mName = name;
+        this.mCause = cause;
+        this.mNumber = number;
         instances.add(this);
         mId = instances.size();
         mMembers = new ArrayList<Members>();
     }
 
-    public String getmName() {
+    public String getName() {
         return mName;
     }
 
-    public void setmName(String mName) {
-        this.mName = mName;
+    public void setName(String name) {
+        this.mName = name;
     }
 
-    public String getmCause() {
+    public String getCause() {
         return mCause;
     }
 
-    public void setmCause(String mCause) {
-        this.mCause = mCause;
+    public void setCause(String cause) {
+        this.mCause = cause;
     }
 
-    public int getmNumber() {
+    public int getNumber() {
         return mNumber;
     }
 
-    public void setmNumber(int mNumber) {
-        this.mNumber = mNumber;
+    public void setNumber(int number) {
+        this.mNumber = number;
+    }
+
+    public static List<Teams> all(){
+        return instances;
+    }
+
+    public int getId(){
+        return mId;
+    }
+
+    public void setId(int id) {
+        this.mId = id;
+    }
+
+    public static Teams find(int id){
+        return instances.get(id-1);
+    }
+
+    public static void clearTeams(){
+        instances.clear();
+    }
+    public void deleteTeam(){
+        instances.remove(mId-1);
+    }
+
+    public List<Members> getMembers() {
+        return mMembers;
+    }
+
+    public void addMember(Members member){
+        mMembers.add(member);
+    }
+
+    public void removeMember(Members member){
+        mMembers.remove(member);
     }
 }
