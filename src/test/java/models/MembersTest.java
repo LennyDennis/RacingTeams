c98467cccd3ec4df287612405c9a6aabd69d7a91
LennyDennis <lennydennis@gmail.com>
2020-02-02 18:13:24 +0300

    Add methods to the Members class methods

--- src/test/java/models/MembersTest.java
+++ src/test/java/models/MembersTest.java
@@ -1,7 +1,66 @@
 package models;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class MembersTest {
+public class MembersTest {
+    @After
+    public void tearDown(){
+        Members.clearMembers();
+    }
+
+    @Test
+    public void Members_getTheNameValue_name(){
+        Members member = new Members("Lenny",23,"DRS","Corners");
+        assertEquals("Lenny",member.getName());
+
+    }
+    @Test
+    public void Members_getTheAgeValue_age(){
+        Members member = new Members("Lenny",23,"DRS","Corners");
+        assertEquals(23,member.getAge());
+
+    }
+    @Test
+    public void Members_getThePowerValue_power(){
+        Members member = new Members("Lenny",23,"DRS","Corners");
+        assertEquals("DRS",member.getPower());
+
+    }
+    @Test
+    public void Members_getWeaknessValue_weakness(){
+        Members member = new Members("Lenny",23,"DRS","Corners");
+        assertEquals("Corners",member.getWeakness());
+
+    }
+    @Test
+    public void Member_getAllMemberInstances_members(){
+        Members member1 = new Members("Lenny",23,"DRS","Corners");
+        Members member2 = new Members("Lenny",23,"DRS","Corners");
+        assertTrue(Members.all().contains(member1));
+        assertTrue(Members.all().contains(member2));
+    }
+
+    @Test
+    public void Member_getIdOfTheMember_1(){
+        Members member = new Members("Lenny",23,"DRS","Corners");
+        assertEquals(1,member.getId());
+    }
+
+    @Test
+    public void Member_deletesAllMembers_zeroSize(){
+        Members member1 = new Members("Lenny",23,"DRS","Corners");
+        Members member2 = new Members("Lenny",23,"DRS","Corners");
+        Members.clearMembers();
+        assertEquals(0,Members.all().size());
+    }
+
+    @Test
+    public void Member_getSpecificMember_2(){
+        Members member1 = new Members("Lenny",23,"DRS","Corners");
+        Members member2 = new Members("Lenny",23,"DRS","Corners");
+        assertEquals(member2,member2.find(member2.getId()));
+    }
 
 }
